da6068f1ce82b042a52173547ad8d289f8107f97
GlaciousV <steve.g.liong@gmail.com>
2026-08-24 15:19:39 +0700

    pesan commit

---
+++ a.java
@@ -0,0 +1,19 @@
+public class a {
+    class node {
+        int head;
+
+        public int getHead() {
+            return head;
+        }
+
+        public void setHead(int head) {
+            this.head = head;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("nama");
+        int a = 2 ;
+        int b = 3;
+    }
+}
